770f4b6de6dee428d3c72d8751b4b88210357586
Matheus Cavalcanti <Matheustcavalcanti@hotmail.com>
2023-04-27 15:04:28 -0300

    Desenvolvendo big-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
     MenuBarComponent,
     SmallCardComponent,
     BigCardComponent, 
-    MenuTitleComponent
+    MenuTitleComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
